57a797660d64172550c46cfc91252c19c04f351d
Po-Cheng Yeh <blp100x@hotmail.com>
2024-02-27 00:17:41 +0800

    feat: Create Server

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+import http from "http";
+import { PORT } from "./constants.js";
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("Hello");
+  res.end();
+});
+
+server.listen(PORT);
 
 // - Create Server
 // - Deal with 404
